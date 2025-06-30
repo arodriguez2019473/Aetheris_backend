2cec234e6cf397b7035bc8769846e17b8c676d07
arodriguez2019473 <arodriguez2019473@kinal.edu.gt>
2025-06-30 11:17:47 -0600

    hu

--- app.py
+++ app.py
@@ -21,3 +21,5 @@ app.register_blueprint(registro_bp, url_prefix='/registroFacturas')
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+# asdas
